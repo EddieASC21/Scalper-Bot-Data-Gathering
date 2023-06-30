b882885378df0c063aac6b9c15942dbd89222b21
jujud4 <84820215+jujud4@users.noreply.github.com>
2023-06-30 14:40:29 -0400

    Update #APP Scalper Bot Data Gathering.py

--- #APP Scalper Bot Data Gathering.py
+++ #APP Scalper Bot Data Gathering.py
@@ -1,3 +1,23 @@
-#AMP Scalper Bot Data Gathering
-import numpy
-print("Hello World")
+#APP Scalper Bot Data Gathering
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+import random
+
+#Establish Customer Classes
+class Customer():
+    def __init__(self, ArrivalT, ServiceT):
+        self.ArrivalT = None
+        self.ServiceT = None
+    
+class Human(Customer):
+    def __init__(self, ArrivalT, ServiceT):
+        super().__init__(ArrivalT= ArrivalT, ServiceT= ServiceT)
+
+class Bot(Customer):
+    def __init__(self, ArrivalT, ServiceT):
+        super().__init__(ArrivalT= ArrivalT, ServiceT= ServiceT)
+
+class Autofill(Customer):
+    def __init__(self, ArrivalT, ServiceT):
+        super().__init__(ArrivalT= ArrivalT, ServiceT= ServiceT)
