bdb2bd0e282b4e5e36cdbc838d6c444056adb8ba
jujud4 <84820215+jujud4@users.noreply.github.com>
2023-07-03 13:59:17 -0400

    Update #APP Scalper Bot Data Gathering.py

--- #APP Scalper Bot Data Gathering.py
+++ #APP Scalper Bot Data Gathering.py
@@ -3,21 +3,58 @@ import numpy as np
 import seaborn as sns
 import matplotlib as plt
 import random
+import time
 
 #Establish Customer Classes
 class Customer():
-    def __init__(self, ArrivalT, ServiceT):
+    def __init__(self, ArrivalT, ServiceT, status, served):
         self.ArrivalT = None
         self.ServiceT = None
+        self.status = status
+        self.served = False
+    #Calculate Arrival time, Service time per class
+
+    def arrivalTime(self, status):
+        if status == "human":
+            self.ArrivalT = random.randint(30,60)
+        if status == "autofill":
+            self.ArrivalT = random.randint(45,75)
+        if self == "bots": 
+            self.ArrivalT = random.randint(90,180)
     
-class Human(Customer):
-    def __init__(self, ArrivalT, ServiceT):
-        super().__init__(ArrivalT= ArrivalT, ServiceT= ServiceT)
+    def serviceTime(self, status):
+        if status == "human":
+            self.ServiceT = random.randint(60,120)
+        if status == "autofill":
+            self.ServiceT = random.randint(15,30)
+        if status == "bots":
+            self.ServiceT = random.randint(5,10)
 
-class Bot(Customer):
-    def __init__(self, ArrivalT, ServiceT):
-        super().__init__(ArrivalT= ArrivalT, ServiceT= ServiceT)
 
-class Autofill(Customer):
-    def __init__(self, ArrivalT, ServiceT):
-        super().__init__(ArrivalT= ArrivalT, ServiceT= ServiceT)
+
+class Queue():
+    def __init__(self, waitList):
+        self.WaitList = {}
+
+    def waitTime(self, waitList):
+        if len(self.WaitList) == 0:
+            return 0 
+        else:
+            pass
+
+
+
+
+
+
+wait = []
+for i in range(random.randint(75,100)):    
+    wait.append(Customer(Customer.arrivalTime(Customer.self, "human"), Customer.serviceTime(Customer.self, "human"), "human", False))       
+
+for i in range(random.randint(50,75)):    
+    wait.append(Customer(Customer.arrivalTime(Customer.self, "autofill"), Customer.serviceTime(Customer.self, "autofill"), "autofill", False))       
+          
+for i in range(random.randint(10,25)):    
+    wait.append(Customer(Customer.arrivalTime(Customer.self, "bots"), Customer.serviceTime(Customer.self, "bots"), "bots", False))       
+    
+random.shuffle(wait)
